74380a7a72baaa5e9460840502ae00f2c8812245
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-10-23 16:53:43 -0300

    try using fragment

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+// biome-ignore-all lint/complexity/noUselessFragments: true
 import Link from "next/link";
 import {
   DropdownMenu,
@@ -8,18 +9,20 @@ import {
 
 export default function HomePage() {
   return (
-    <nav className="flex items-center justify-between">
-      <h1>home navbar</h1>
-      <DropdownMenu>
-        <DropdownMenuTrigger className="bg-black rounded-lg text-white px-4 py-2">
-          Open Dropdown
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem asChild>
-            <Link href="/settings">Settings</Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </nav>
+    <>
+      <nav className="flex items-center justify-between">
+        <h1>home navbar</h1>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="bg-black rounded-lg text-white px-4 py-2">
+            Open Dropdown
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem asChild>
+              <Link href="/settings">Settings</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
+    </>
   );
 }
